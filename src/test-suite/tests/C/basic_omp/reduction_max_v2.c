856620da2567b8b1875e00d28b68fbeca0be18f5
Jannek <squar@informatik.uni-hamburg.de>
2022-09-13 16:07:50 +0200

    Add new trivial test case

---
+++ src/test-suite/tests/C/basic_omp/reduction_max_v2.c
@@ -0,0 +1,19 @@
+// PASS: *
+// RUN: ${CATO_ROOT}/src/scripts/cexecute_pass.py %s -o %t
+// RUN: diff <(mpirun -np 4 %t) %s.reference_output
+#include <stdio.h>
+#include <omp.h>
+
+int main()
+{
+    int foo = 0;
+    int bar = 0;
+
+    #pragma omp parallel reduction(max:foo,bar)
+    {
+        foo = omp_get_thread_num();
+        bar = omp_get_num_threads();
+    }
+
+    printf("Foo: %d\nBar: %d\n", foo, bar);
+}
